68b5849f9b24b58e5c2b3f88aa9eb8887710a570
Michael Lei <michaelleie0ca@gmail.com>
2024-08-12 16:58:39 -0700

    Add native wind and setup rnr

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+	return (
+		<View className="w-full h-full p-4">
+			<Text className="text-foreground">Testing</Text>
+		</View>
+	);
+}
