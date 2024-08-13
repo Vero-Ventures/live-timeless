244c8c4cf9b0e886863aa97e7d62663f25185c28
scottchen98 <scott.chen1117@gmail.com>
2024-08-12 23:22:02 -0700

    update HomePage component to use SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomePage() {
-	return (
-		<View className="w-full h-full p-4">
-			<Text className="text-foreground">Testing</Text>
-		</View>
-	);
+  return (
+    <SafeAreaView className="h-full">
+      <View className="h-full p-4">
+        <Text className="text-foreground">Testing</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
