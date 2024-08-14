9402e7a8ed1626687767a4dd8bb18c265fac0a05
Michael Lei <michaelleie0ca@gmail.com>
2024-08-13 18:47:05 -0700

    Add eslint

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
+/* eslint-env node */
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
