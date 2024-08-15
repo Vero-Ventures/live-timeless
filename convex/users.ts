b55e9090099e421cf920ab438e1b7bc46cc2fd08
scottchen98 <scott.chen1117@gmail.com>
2024-08-15 16:27:56 -0700

    add getUser and createUser functions

---
+++ convex/users.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { internalMutation, query } from "./_generated/server";
+
+export const getUser = query({
+  args: { userId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("kindeId"), args.userId));
+    return user;
+  },
+});
+
+export const createUser = internalMutation({
+  args: {
+    kindeId: v.string(),
+    firstName: v.string(),
+    lastName: v.string(),
+    email: v.string(),
+  },
+  async handler(ctx, args) {
+    const userId = await ctx.db.insert("users", args);
+    return userId;
+  },
+});
