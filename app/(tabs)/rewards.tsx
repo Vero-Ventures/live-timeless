9f1a0c08a82c89f85b5251d7a2e9891c30691328
scottchen98 <scott.chen1117@gmail.com>
2024-08-29 22:35:52 -0700

    add gift icon
    Co-authored-by: Michael Lei <michaelleie0ca@gmail.com>

--- app/(tabs)/rewards.tsx
+++ app/(tabs)/rewards.tsx
@@ -2,12 +2,26 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Text } from "~/components/ui/text";
+import SearchInput from "~/components/search-input";
+import { Coins } from "~/lib/icons/Coins";
 
 export default function RewardsPage() {
   return (
     <SafeAreaView className="h-full">
-      <View className="h-full gap-4 p-4">
-        <Text>Rewards Page</Text>
+      <View className="gap-10 p-4">
+        <View className="flex flex-row items-center gap-4">
+          <Text>
+            <Coins size={40} />
+          </Text>
+          <View className="gap-2">
+            <Text className="text-2xl font-bold">100</Text>
+            <Text className="text-md">LT Token Balance</Text>
+          </View>
+        </View>
+        <View className="gap-3">
+          <Text className="text-2xl font-semibold">Available Rewards</Text>
+          <SearchInput />
+        </View>
       </View>
     </SafeAreaView>
   );
