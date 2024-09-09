f9a41afd80b0ef158800d4e4e9919ca1ac5e89e2
Michael Lei <michaelleie0ca@gmail.com>
2024-09-09 16:09:48 -0700

    Add calendar strip comp
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

--- app/(tabs)/goals.tsx
+++ app/(tabs)/goals.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "convex/react";
 import type { FunctionReturnType } from "convex/server";
-import { FlatList, Pressable, View } from "react-native";
+import { FlatList, Pressable, View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   Card,
@@ -13,30 +13,64 @@ import { api } from "~/convex/_generated/api";
 import { Button } from "~/components/ui/button";
 import { Link } from "expo-router";
 import { useUserProfile } from "~/providers/kindeUserProfileProvider";
+import {
+  useEffect,
+  useRef,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
+import { fontFamily } from "~/lib/font";
+import { Plus } from "lucide-react-native";
+import { Separator } from "~/components/ui/separator";
 
 export default function GoalsPage() {
   const { user: userProfile } = useUserProfile();
   const goals = useQuery(api.goals.list, { userId: userProfile?.id ?? "" });
+  const { today, tomorrow, yesterday } = getTodayYesterdayTomorrow();
+  const [selectedDate, setSelectedDate] = useState(today);
 
   return (
-    <SafeAreaView
-      style={{
-        height: "100%",
-        backgroundColor: "#082139",
-      }}
-    >
-      <View className="h-full gap-4 px-4 pb-20">
-        <FlatList
-          className="create-goal-form"
-          data={goals}
-          ItemSeparatorComponent={() => <View className="p-2" />}
-          renderItem={({ item }) => <GoalItem goal={item} />}
-          keyExtractor={(g) => g._id}
-        />
-        <View>
+    <SafeAreaView style={{ height: "100%", backgroundColor: "#082139" }}>
+      <View className="flex h-full justify-between">
+        <View className="flex-1 px-4">
+          <Text className="mb-2 text-sm uppercase text-gray-500">
+            {selectedDate.toDateString() === today.toDateString()
+              ? "Today"
+              : selectedDate.toDateString() === yesterday.toDateString()
+                ? "Yesterday"
+                : selectedDate.toDateString() === tomorrow.toDateString()
+                  ? "Tomorrow"
+                  : selectedDate.toLocaleDateString("en-US", {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+          </Text>
+          <Text
+            className="text-2xl"
+            style={{
+              fontFamily: fontFamily.openSans.bold,
+            }}
+          >
+            Goals
+          </Text>
+          <FlatList
+            data={goals}
+            ItemSeparatorComponent={() => <View className="p-2" />}
+            renderItem={({ item }) => <GoalItem goal={item} />}
+            keyExtractor={(g) => g._id}
+          />
+        </View>
+        <View className="mb-20 flex-row items-center gap-2 bg-[#0b1a28] px-4">
+          <CalendarStrip
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+          />
+          <Separator orientation="vertical" className="mx-2" />
           <Link href="/goals/create" asChild>
-            <Button size="lg">
-              <Text>Set Goal</Text>
+            <Button size="icon" className="h-14 w-14 rounded-full">
+              <Plus color="#fff" size={30} />
             </Button>
           </Link>
         </View>
@@ -69,4 +103,66 @@ function GoalItem({
       </Pressable>
     </Link>
   );
+}
+
+function CalendarStrip({
+  selectedDate,
+  setSelectedDate,
+}: {
+  selectedDate: Date;
+  setSelectedDate: Dispatch<SetStateAction<Date>>;
+}) {
+  const scrollViewRef = useRef<ScrollView>(null);
+  const { tomorrow } = getTodayYesterdayTomorrow();
+  const dates = Array.from({ length: 17 }, (_, i) => {
+    const date = new Date(tomorrow);
+    date.setDate(tomorrow.getDate() - i);
+    return date;
+  }).reverse();
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (scrollViewRef.current) {
+        scrollViewRef.current.scrollTo({ x: 15 * 63, animated: true });
+      }
+    }, 100);
+    return () => clearTimeout(id);
+  }, []);
+
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="py-4"
+      ref={scrollViewRef}
+    >
+      {dates.map((date, index) => (
+        <Pressable
+          key={index}
+          className={`mx-2 items-center ${
+            date.toDateString() === selectedDate.toDateString()
+              ? "rounded-md bg-primary p-2"
+              : "p-2"
+          }`}
+          onPress={() => setSelectedDate(date)}
+        >
+          <Text className="text-sm">
+            {date.toLocaleDateString("en-US", { weekday: "short" })}
+          </Text>
+          <View className="w-14 items-center justify-center">
+            <Text className="text-sm font-bold">{date.getDate()}</Text>
+          </View>
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+}
+
+function getTodayYesterdayTomorrow() {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+  return { today, tomorrow, yesterday };
 }
