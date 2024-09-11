17dfd20e55a3bd6645effa28184b6f4a11dd9633
scottchen98 <scott.chen1117@gmail.com>
2024-09-11 08:45:35 -0700

    add DailyRepeat and MonthlyRepeat options to CreateGoalFormState
    Co-authored-by: Michael Lei <michaelleie0ca@gmail.com>

--- app/goals/create/create-goal-store.ts
+++ app/goals/create/create-goal-store.ts
@@ -1,12 +1,24 @@
 import { create } from "zustand";
 
 type TimeOfDay = "Morning" | "Afternoon" | "Evening";
+export type DailyRepeat =
+  | "Sunday"
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday";
 
 interface CreateGoalFormState {
   name: string;
   setName: (name: string) => void;
   timeOfDay: TimeOfDay[];
   setTimeOfDay: (timeOfDay: TimeOfDay[]) => void;
+  dailyRepeat: DailyRepeat[];
+  setDailyRepeat: (dailyRepeat: DailyRepeat[]) => void;
+  monthlyRepeat: number[];
+  setMonthlyRepeat: (monthlyRepeat: number[]) => void;
 }
 
 export const useCreateGoalFormStore = create<CreateGoalFormState>()((set) => ({
@@ -14,4 +26,16 @@ export const useCreateGoalFormStore = create<CreateGoalFormState>()((set) => ({
   setName: (name) => set({ name }),
   timeOfDay: ["Morning", "Afternoon", "Evening"],
   setTimeOfDay: (timeOfDay) => set({ timeOfDay }),
+  dailyRepeat: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  setDailyRepeat: (dailyRepeat) => set({ dailyRepeat }),
+  monthlyRepeat: Array.from({ length: 31 }, (_, index) => index + 1),
+  setMonthlyRepeat: (monthlyRepeat) => set({ monthlyRepeat }),
 }));
