4d2127fc8a48f89f693da7b0f4ada0b691a3d6bc
scottchen98 <scott.chen1117@gmail.com>
2024-09-11 09:35:08 -0700

    add TimeItem and AddItem components
    Co-authored-by: Michael Lei <michaelleie0ca@gmail.com>

--- app/goals/create/reminders.tsx
+++ app/goals/create/reminders.tsx
@@ -1,7 +1,12 @@
+import { type LucideIcon } from "lucide-react-native";
+
 import { Stack } from "expo-router";
 import { View } from "react-native";
 import { Text } from "~/components/ui/text";
 import { fontFamily } from "~/lib/font";
+import { X } from "~/lib/icons/X";
+import { Clock } from "~/lib/icons/Clock";
+import { Plus } from "~/lib/icons/Plus";
 
 export default function Reminders() {
   return (
@@ -20,8 +25,63 @@ export default function Reminders() {
           headerBackTitleVisible: false,
         }}
       />
-      <View>
-        <Text>Reminders</Text>
+      <View className="p-5">
+        <View className="gap-3">
+          <Text
+            className="text-sm text-muted-foreground"
+            style={{
+              fontFamily: fontFamily.openSans.semiBold,
+              marginLeft: 8,
+            }}
+          >
+            TIME
+          </Text>
+          <View className="rounded-xl bg-[#0e2942]">
+            <TimeItem Icon={Clock} time="3:10 PM" />
+            <TimeItem Icon={Clock} time="3:10 PM" />
+            <TimeItem Icon={Clock} time="3:10 PM" />
+            <AddItem Icon={Plus} label="Add" />
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+
+function TimeItem({ Icon, time }: { Icon: LucideIcon; time: string }) {
+  return (
+    <>
+      <View className="flex flex-row items-center gap-4 p-5">
+        <Icon />
+        <View className="flex flex-1 flex-row items-center justify-between">
+          <Text
+            style={{
+              fontFamily: fontFamily.openSans.semiBold,
+            }}
+          >
+            {time}
+          </Text>
+
+          <X className="font-bold text-primary" size={20} />
+        </View>
+      </View>
+    </>
+  );
+}
+
+function AddItem({ Icon, label }: { Icon: LucideIcon; label: string }) {
+  return (
+    <>
+      <View className="flex flex-row items-center gap-4 p-5">
+        <Icon />
+        <Text
+          className="text-primary"
+          style={{
+            fontFamily: fontFamily.openSans.semiBold,
+          }}
+        >
+          {label}
+        </Text>
       </View>
     </>
   );
