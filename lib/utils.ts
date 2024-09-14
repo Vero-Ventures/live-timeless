3410aedeef96c54866d4f1fd2355539bb291a8df
scottchen98 <scott.chen1117@gmail.com>
2024-09-13 17:39:13 -0700

    add utility functions for array range generation and string capitalization
    Co-authored-by: Michael Lei <michaelleie0ca@gmail.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const arrayRange = (start: number, stop: number, step: number) =>
+  Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, index) => start + index * step
+  );
+
+export const capitalize = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
