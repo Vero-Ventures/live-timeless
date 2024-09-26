2196e3198518b6b7f88622569c6b7b1f62c10464
bli129 <bennyli.byli@gmail.com>
2024-09-25 22:45:08 -0700

    Implemented backend for goal update

--- convex/goals.ts
+++ convex/goals.ts
@@ -1,6 +1,13 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
+export const getGoalById = query({
+  args: { goalId: v.id("goals") },
+  handler: async (ctx, { goalId }) => {
+    return await ctx.db.get(goalId); // Fetch the goal by ID
+  },
+});
+
 export const listGoals = query(async ({ db }) => {
   const goals = await db.query("goals").collect();
   return goals;
@@ -22,6 +29,24 @@ export const createGoal = mutation({
   },
   handler: async (ctx, args) => {
     await ctx.db.insert("goals", args);
+  },
+});
+
+export const updateGoal = mutation({
+  args: {
+    goalId: v.id("goals"),  // ID of the goal to update
+    name: v.string(),
+    selectedIcon: v.string(),
+    selectedIconColor: v.string(),
+    timeOfDay: v.array(v.string()),
+    timeReminder: v.string(),
+    repeatType: v.string(),
+    dailyRepeat: v.array(v.string()),
+    monthlyRepeat: v.array(v.float64()),
+    intervalRepeat: v.float64(),
+  },
+  handler: async (ctx, { goalId, ...updates }) => {
+    await ctx.db.patch(goalId, updates); // Patch the goal in the database
   },
 });
 
