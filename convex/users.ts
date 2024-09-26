ef61adf4aeebc959f8acc0d2217259f1e04b36ef
Conrad Christian <cchristian5@my.bcit.ca>
2024-09-25 21:43:54 -0700

    Add db mutation to patch user information

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,7 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { query } from "./_generated/server";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const currentUser = query({
   args: {},
@@ -19,5 +21,19 @@ export const currentUser = query({
       height: user?.height,
       weight: user?.weight,
     };
+  },
+});
+
+export const updateTask = mutation({
+  args: { id: v.id("users") },
+  handler: async (ctx, args) => {
+    const { id } = args;
+
+    console.log(await ctx.db.get(id));
+
+    // Add `tag` and overwrite `status`:
+    // await ctx.db.patch(id, { tag: "bar", status: { archived: true } });
+    // console.log(await ctx.db.get(id));
+    // { text: "foo", tag: "bar", status: { archived: true }, _id: ... }
   },
 });
