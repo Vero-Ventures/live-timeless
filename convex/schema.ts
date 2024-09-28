ad08f8b7d1e0032a9e91ce6c0c9fc043091db2ea
mikaem <mkmanaligod@gmail.com>
2024-09-28 11:16:52 -0700

    update users schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,21 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
 
 export default defineSchema({
   ...authTables,
+  users: defineTable({
+    _id: v.id("users"),
+    name: v.optional(v.string()),
+    email: v.optional(v.string()),
+    dob: v.optional(v.string()),
+    gender: v.optional(v.string()),
+    height: v.optional(v.number()),
+    weight: v.optional(v.number()),
+
+    emailVerificationTime: v.optional(v.number()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.number()),
+    isAnonymous: v.optional(v.boolean()),
+  }).index("email", ["email"]),
 });
