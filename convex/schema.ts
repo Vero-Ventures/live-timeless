4ec86d0bc3af360698525d30879c80f9039e3d04
mikaem <mkmanaligod@gmail.com>
2024-10-01 22:14:53 -0700

    update schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,13 +6,12 @@ export default defineSchema({
   ...authTables,
   users: defineTable({
     _id: v.id("users"),
+    email: v.string(),
     name: v.optional(v.string()),
-    email: v.optional(v.string()),
     dob: v.optional(v.string()),
     gender: v.optional(v.string()),
     height: v.optional(v.number()),
     weight: v.optional(v.number()),
-
     emailVerificationTime: v.optional(v.number()),
     phone: v.optional(v.string()),
     phoneVerificationTime: v.optional(v.number()),
