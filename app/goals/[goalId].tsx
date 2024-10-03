daeb9fd4b5c2e302408422c804b6cfb4c6829f89
scottchen98 <scott.chen1117@gmail.com>
2024-10-02 17:08:14 -0700

    add left and right headers

--- app/goals/[goalId].tsx
+++ app/goals/[goalId].tsx
@@ -1,6 +1,7 @@
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { useQuery } from "convex/react";
-import { Stack, useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Pressable, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import { api } from "~/convex/_generated/api";
 import { Id } from "~/convex/_generated/dataModel";
@@ -10,6 +11,7 @@ import { ChevronLeft } from "~/lib/icons/ChevronLeft";
 export default function GoalScreen() {
   const { goalId } = useLocalSearchParams<{ goalId: Id<"goals"> }>();
   const goal = useQuery(api.goals.getGoalById, { goalId });
+  const router = useRouter();
 
   return (
     <View className="h-full gap-4 bg-background p-4">
@@ -21,15 +23,30 @@ export default function GoalScreen() {
           headerTintColor: "#fff",
           headerTitle: "",
           headerLeft: () => (
-            <View className="flex flex-row items-center justify-center">
+            <Pressable
+              className="flex flex-row items-center justify-center"
+              hitSlop={20}
+              onPress={() => router.navigate("/goals")}
+            >
               <ChevronLeft color="#fff" size={35} />
               <Text
                 className="text-xl"
                 style={{ fontFamily: fontFamily.openSans.bold }}
               >
-                {goal?.name}
+                {goal ? goal.name : "Goal"}
               </Text>
-            </View>
+            </Pressable>
+          ),
+          headerRight: () => (
+            <Pressable
+              className="rounded-full bg-[#fff] p-1"
+              hitSlop={20}
+              onPress={() =>
+                router.navigate(`/goals/edit-goal-page?goalId=${goalId}`)
+              }
+            >
+              <FontAwesome5 name="ellipsis-h" size={16} color="#0b1a28" />
+            </Pressable>
           ),
         }}
       />
