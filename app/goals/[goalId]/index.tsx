5032277677dd37493a644aaf1f1ae3b00e4bad83
Michael Lei <michaelleie0ca@gmail.com>
2024-10-02 20:27:03 -0700

    Add zeego dropdown menu
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

--- app/goals/[goalId]/index.tsx
+++ app/goals/[goalId]/index.tsx
@@ -1,15 +1,26 @@
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { Stack, useLocalSearchParams, router } from "expo-router";
 import { Pressable, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import { api } from "~/convex/_generated/api";
 import { Id } from "~/convex/_generated/dataModel";
 import { fontFamily } from "~/lib/font";
+import * as DropdownMenu from "zeego/dropdown-menu";
 
 export default function GoalScreen() {
   const { goalId } = useLocalSearchParams<{ goalId: Id<"goals"> }>();
   const goal = useQuery(api.goals.getGoalById, { goalId });
+  const deleteGoal = useMutation(api.goals.deleteGoal);
+
+  const handleDelete = async () => {
+    try {
+      await deleteGoal({ goalId });
+      router.dismiss();
+    } catch (error) {
+      console.error("Error deleting goal:", error);
+    }
+  };
 
   return (
     <View className="h-full gap-4 bg-background p-4">
@@ -29,17 +40,44 @@ export default function GoalScreen() {
           ),
           headerBackTitleVisible: false,
           headerRight: () => (
-            <Pressable
-              hitSlop={20}
-              onPress={() =>
-                router.navigate({
-                  pathname: "/goals/[goalId]/edit",
-                  params: { goalId },
-                })
-              }
-            >
-              <FontAwesome5 name="edit" size={20} color="#fff" />
-            </Pressable>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger asChild>
+                <Pressable hitSlop={20}>
+                  <FontAwesome5 name="ellipsis-h" size={20} color="#fff" />
+                </Pressable>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content key="actions">
+                <DropdownMenu.Item
+                  onSelect={() =>
+                    router.navigate({
+                      pathname: "/goals/[goalId]/edit",
+                      params: { goalId },
+                    })
+                  }
+                  key="edit-goal"
+                >
+                  <DropdownMenu.ItemIcon
+                    ios={{
+                      name: "pencil.line",
+                    }}
+                  />
+                  <DropdownMenu.ItemTitle>Edit Goal</DropdownMenu.ItemTitle>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item
+                  onSelect={handleDelete}
+                  destructive
+                  key="delete-goal"
+                >
+                  <DropdownMenu.ItemIcon
+                    ios={{
+                      name: "trash",
+                    }}
+                  />
+                  <DropdownMenu.ItemTitle>Delete Goal</DropdownMenu.ItemTitle>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           ),
         }}
       />
