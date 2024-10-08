bfa260e490a231adf6ccece9a806e1db88aaffda
Michael Lei <michaelleie0ca@gmail.com>
2024-10-07 19:34:17 -0700

    We are already embeding the fonts with the `expo-font` config plugin so we don't need to use `use-fonts` hook here

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,22 +14,6 @@ import { NAV_THEME } from "~/lib/constants";
 import { useColorScheme } from "~/hooks/useColorScheme";
 import { PortalHost } from "@rn-primitives/portal";
 
-import {
-  useFonts,
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-  OpenSans_800ExtraBold,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold_Italic,
-  OpenSans_800ExtraBold_Italic,
-} from "@expo-google-fonts/open-sans";
-
 const DARK_THEME: Theme = {
   dark: true,
   colors: NAV_THEME.dark,
@@ -56,20 +40,6 @@ const secureStorage = {
 export default function RootLayout() {
   const { colorScheme, setColorScheme } = useColorScheme();
   const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
-  let [fontsLoaded] = useFonts({
-    OpenSans_300Light,
-    OpenSans_400Regular,
-    OpenSans_500Medium,
-    OpenSans_600SemiBold,
-    OpenSans_700Bold,
-    OpenSans_800ExtraBold,
-    OpenSans_300Light_Italic,
-    OpenSans_400Regular_Italic,
-    OpenSans_500Medium_Italic,
-    OpenSans_600SemiBold_Italic,
-    OpenSans_700Bold_Italic,
-    OpenSans_800ExtraBold_Italic,
-  });
 
   React.useEffect(() => {
     (async () => {
@@ -97,10 +67,6 @@ export default function RootLayout() {
   }, [colorScheme, setColorScheme]);
 
   if (!isColorSchemeLoaded) {
-    return null;
-  }
-
-  if (!fontsLoaded) {
     return null;
   }
 
