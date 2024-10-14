dc1d34e1ad1ae58bfb6eab154a2bbdb5aff6d4b9
bli129 <bennyli.byli@gmail.com>
2024-10-14 05:15:10 -0700

    Fixed eslint error

--- app/goals/[goalId]/[goalLogId]/start/index.tsx
+++ app/goals/[goalId]/[goalLogId]/start/index.tsx
@@ -216,7 +216,7 @@ export default function StartGoalScreen() {
       )}
 
       {/* Show Timer for Duration-based Goals */}
-      {isDurationGoal && (
+      {!!isDurationGoal && (
         <View className="items-center justify-center">
           {/* Timer Display */}
           <Text
