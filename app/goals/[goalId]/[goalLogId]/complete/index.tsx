89acce6cbdb448099feb34692953bc193b7babfe
mikaem <mkmanaligod@gmail.com>
2024-10-14 20:41:23 -0700

    Enhance goal sharing message and add user feedback

--- app/goals/[goalId]/[goalLogId]/complete/index.tsx
+++ app/goals/[goalId]/[goalLogId]/complete/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "convex/react";
 import { Stack, useLocalSearchParams, router } from "expo-router";
-import { Pressable, View, Share } from "react-native";
+import { Pressable, View, Share, Alert } from "react-native";
 import { Text } from "~/components/ui/text";
 import { api } from "~/convex/_generated/api";
 import { fontFamily } from "~/lib/font";
@@ -8,25 +8,26 @@ import type { Id } from "~/convex/_generated/dataModel";
 
 export default function GoalCompletionScreen() {
   const { goalId } = useLocalSearchParams<{ goalId: Id<"goals"> }>();
-
-  // Fetch all goalLogs associated with this goalId
+  const goal = useQuery(api.goals.getGoalById, { goalId });
   const goalLogs = useQuery(api.goalLogs.getGoalLogsbyGoalId, { goalId });
-
-  if (!goalLogs) {
+  if (!goal || !goalLogs) {
     return <Text>Loading...</Text>;
   }
 
-  // Calculate total, completed, and remaining goalLogs
   const totalLogs = goalLogs.length;
   const completedLogs = goalLogs.filter((log) => log.isComplete).length;
 
+  // Sharing Goals
   const handleShare = async () => {
     try {
       await Share.share({
-        message: `I just completed ${completedLogs} day(s) out of ${totalLogs} towards my goal! #goals`,
+        message: `I just completed ${completedLogs} day(s) out of ${totalLogs} towards my goal
+        of ${goal.name}! #goals #wellness`,
       });
+      Alert.alert("Success", "Your progress has been shared successfully.");
     } catch (error) {
       console.error("Error sharing:", error);
+      Alert.alert("Error", "There was an error sharing your progress. Try again later.");
     }
   };
 
