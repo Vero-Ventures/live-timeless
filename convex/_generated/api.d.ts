d26729851f2122f26f4f22b802d26ba1ab8ecdb2
Michael Lei <michaelleie0ca@gmail.com>
2024-10-15 18:24:46 -0700

    Update schema

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,52 @@
+/* prettier-ignore-start */
+
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as ResendOTP from "../ResendOTP.js";
+import type * as auth from "../auth.js";
+import type * as challenges from "../challenges.js";
+import type * as goalLogs from "../goalLogs.js";
+import type * as goals from "../goals.js";
+import type * as http from "../http.js";
+import type * as users from "../users.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  ResendOTP: typeof ResendOTP;
+  auth: typeof auth;
+  challenges: typeof challenges;
+  goalLogs: typeof goalLogs;
+  goals: typeof goals;
+  http: typeof http;
+  users: typeof users;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+/* prettier-ignore-end */
