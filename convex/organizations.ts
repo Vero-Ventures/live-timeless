3426cb954b5596e12ecea873e5707b78c15b1aa4
scottchen98 <scott.chen1117@gmail.com>
2024-10-22 17:15:57 -0700

    remove createOrganization mutation

--- convex/organizations.ts
+++ convex/organizations.ts
@@ -20,33 +20,6 @@ export const getOrganizationBySlug = query({
   },
 });
 
-export const createOrganization = mutation({
-  args: {
-    name: v.string(),
-    slug: v.string(),
-    logo: v.optional(v.string()),
-    metadata: v.optional(v.string()),
-  },
-  handler: async (ctx, args) => {
-    const userId = await getAuthUserId(ctx);
-    if (userId === null) {
-      return null;
-    }
-
-    const member = await ctx.db
-      .query("members")
-      .filter((q) => q.eq(q.field("userId"), userId))
-      .collect()
-      .then((res) => res.at(0));
-    if (!member) {
-      throw new Error("Not a member of any organization");
-    }
-
-    const organizationId = await ctx.db.insert("organizations", args);
-    return organizationId;
-  },
-});
-
 export const updateOrganization = mutation({
   args: {
     organizationId: v.id("organizations"),
