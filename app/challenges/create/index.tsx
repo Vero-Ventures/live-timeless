cf915e804ddb2c7d3c78be1ba30b9d8eb5c0913f
Conrad Christian <cchristian5@my.bcit.ca>
2024-11-16 02:33:43 -0800

    Add working fields for challenge generation

--- app/challenges/create/index.tsx
+++ app/challenges/create/index.tsx
@@ -6,7 +6,9 @@ import { api } from "~/convex/_generated/api";
 import { useMutation } from "convex/react";
 import { fontFamily } from "~/lib/font";
 import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 import { useChallengeFormStore } from "./challenge-store";
+import { ScheduleStartDate, ScheduleEndDate} from "../schedule-date";
 import { useShallow } from "zustand/react/shallow";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
@@ -36,10 +38,22 @@ function CreateChallengeForm() {
   const [
     name,
     setName,
+    description,
+    setDescription,
+    startDate,
+    endDate,
+    points,
+    setPoints,
   ] = useChallengeFormStore(
     useShallow((s) => [
       s.name,
       s.setName,
+      s.description,
+      s.setDescription,
+      s.startDate,
+      s.endDate,
+      s.points,
+      s.setPoints
     ])
   )
 
@@ -56,19 +70,40 @@ function CreateChallengeForm() {
           onChangeText={setName}
         />
       </View>
+      <View className="flex flex-row items-center gap-2">
+        <Input
+            className="native:h-16 flex-1 rounded-xl border-0 bg-[#0e2942]"
+            placeholder="Description of Challenge"
+            value={description}
+            onChangeText={setDescription}
+          />
+      </View>
 
+      <ScheduleStartDate />
+      <ScheduleEndDate />
+
+      <View className="gap-2">
+          <Label nativeID="points">Points</Label>
+          <Input
+            placeholder="Enter point value"
+            value={points}
+            onChangeText={setPoints}
+            keyboardType="numeric"
+          />
+        </View>
+     
       <Button onPress={async () => {
         const newChallenge = {
           name,
-          description: "Sample description",
+          description,
           repeat: ["daily"],
           unitType: "General",
           unitValue: 1,
           unit: "times",
           recurrence: "per day",
-          startDate: new Date().getTime(),
-          endDate: new Date().getTime(),
-          points: 10,
+          startDate: startDate.getTime(),
+          endDate: endDate.getTime(),
+          points: points ? parseInt(points) : 0,
         };
         const challengeId = await createChallenge(newChallenge);
         if (!challengeId) {
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,8 +15,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as ResendOTP from "../ResendOTP.js";
 import type * as auth from "../auth.js";
+import type * as challengeGoals from "../challengeGoals.js";
 import type * as challenges from "../challenges.js";
 import type * as emails_LTLoginOTP from "../emails/LTLoginOTP.js";
 import type * as emails_LTUserInvitation from "../emails/LTUserInvitation.js";
@@ -27,6 +27,7 @@ import type * as habitStats from "../habitStats.js";
 import type * as http from "../http.js";
 import type * as invitations from "../invitations.js";
 import type * as organizations from "../organizations.js";
+import type * as ResendOTP from "../ResendOTP.js";
 import type * as singleHabitStats from "../singleHabitStats.js";
 import type * as users from "../users.js";
 
@@ -39,8 +40,8 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  ResendOTP: typeof ResendOTP;
   auth: typeof auth;
+  challengeGoals: typeof challengeGoals;
   challenges: typeof challenges;
   "emails/LTLoginOTP": typeof emails_LTLoginOTP;
   "emails/LTUserInvitation": typeof emails_LTUserInvitation;
@@ -51,6 +52,7 @@ declare const fullApi: ApiFromModules<{
   http: typeof http;
   invitations: typeof invitations;
   organizations: typeof organizations;
+  ResendOTP: typeof ResendOTP;
   singleHabitStats: typeof singleHabitStats;
   users: typeof users;
 }>;
