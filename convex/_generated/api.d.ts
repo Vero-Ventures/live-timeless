adeb8c922558af84587f219538273436e4965216
Michael Lei <michaelleie0ca@gmail.com>
2024-12-04 13:36:23 -0800

    Delete bun lockb

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ResendOTP from "../ResendOTP.js";
 import type * as auth from "../auth.js";
 import type * as challengeGoals from "../challengeGoals.js";
 import type * as challenges from "../challenges.js";
@@ -28,7 +29,6 @@ import type * as habitStats from "../habitStats.js";
 import type * as http from "../http.js";
 import type * as invitations from "../invitations.js";
 import type * as organizations from "../organizations.js";
-import type * as ResendOTP from "../ResendOTP.js";
 import type * as singleHabitStats from "../singleHabitStats.js";
 import type * as users from "../users.js";
 
@@ -41,6 +41,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  ResendOTP: typeof ResendOTP;
   auth: typeof auth;
   challengeGoals: typeof challengeGoals;
   challenges: typeof challenges;
@@ -54,7 +55,6 @@ declare const fullApi: ApiFromModules<{
   http: typeof http;
   invitations: typeof invitations;
   organizations: typeof organizations;
-  ResendOTP: typeof ResendOTP;
   singleHabitStats: typeof singleHabitStats;
   users: typeof users;
 }>;
