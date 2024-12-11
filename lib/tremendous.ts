2f445d0cf62121b0b56afb1cb112637d94a06a10
scottchen98 <scott.chen1117@gmail.com>
2024-12-10 22:29:05 -0800

    Add function to get minimum and maximum product denominations

--- lib/tremendous.ts
+++ lib/tremendous.ts
@@ -1,6 +1,25 @@
 import Big from "big.js";
+import type { ListProductsResponseProductsInnerSkusInner } from "tremendous";
 
 export function convertTokensToDollars(tokens: number) {
   const t = new Big(tokens);
   return t.div(13).round(1).toNumber();
+}
+
+export function getMinAndMaxProductDenominations(
+  arr: ListProductsResponseProductsInnerSkusInner[]
+) {
+  if (!arr.length) {
+    return {
+      minDenomination: 0,
+      maxDenomination: 0,
+    };
+  }
+
+  const firstSku = arr[0];
+  const lastSku = arr[arr.length - 1];
+  return {
+    minDenomination: firstSku.min,
+    maxDenomination: lastSku.max,
+  };
 }
