7fc260093f0783bd2d581d5ba1d08d3e2442eb11
scottchen98 <scott.chen1117@gmail.com>
2024-12-11 15:08:31 -0800

    Add RedeemDialog component for token redemption process

--- app/rewards/[id].tsx
+++ app/rewards/[id].tsx
@@ -5,9 +5,20 @@ import {
   Image,
   SafeAreaView,
   Pressable,
+  FlatList,
 } from "react-native";
 import { Text } from "~/components/ui/text";
 import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Separator } from "~/components/ui/separator";
 import { useEffect, useState } from "react";
 import { useAction, useQuery } from "convex/react";
 import { api } from "~/convex/_generated/api";
@@ -16,12 +27,14 @@ import { ArrowLeft } from "~/lib/icons/ArrowLeft";
 import { MaterialIcons } from "@expo/vector-icons";
 import DOMContent from "~/components/rewards/dom-content";
 import { convertTokensToDollars, getProductSkus } from "~/lib/tremendous";
+import { CircleCheckBig } from "~/lib/icons/CircleCheckBig";
+import { Circle } from "~/lib/icons/Circle";
+import { cn } from "~/lib/utils";
 
 export default function SingleRewardsPage() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const user = useQuery(api.users.currentUser);
   const getProduct = useAction(api.tremendous.getProductAction);
-  const redeemReward = useAction(api.tremendous.redeemRewardAction);
   const [product, setProduct] =
     useState<ListProductsResponseProductsInner | null>(null);
   const [productSkus, setProductSkus] = useState<number[] | null>(null);
@@ -99,19 +112,13 @@ export default function SingleRewardsPage() {
           </View>
           <View className="px-5 pb-10 pt-5">
             {isSufficientDollars ? (
-              <Button
-                size="lg"
-                onPress={async () => {
-                  await redeemReward({
-                    productId: id,
-                    name: user?.name ?? "",
-                    email: user?.email ?? "",
-                    denomination: 30,
-                  });
-                }}
-              >
-                <Text>Redeem</Text>
-              </Button>
+              <RedeemDialog
+                productId={product.disclosure}
+                productName={product.name}
+                productSkus={productSkus}
+                username={user?.name ?? ""}
+                email={user?.email ?? ""}
+              />
             ) : (
               <Text className="text-center text-destructive">
                 You need at least ${minimumSku} worth of LT Tokens to redeem
@@ -126,5 +133,112 @@ export default function SingleRewardsPage() {
         </View>
       )}
     </SafeAreaView>
+  );
+}
+
+function RedeemDialog({
+  productId,
+  productName,
+  productSkus,
+  username,
+  email,
+}: {
+  productId: string;
+  productName: string;
+  productSkus: number[];
+  username: string;
+  email: string;
+}) {
+  const [open, setOpen] = useState(false);
+  const [isRedeeming, setIsRedeeming] = useState(false);
+  const [hasRedeemed, setHasRedeemed] = useState(false);
+  const [unitValue, setUnitValue] = useState(productSkus[0]);
+  const redeemReward = useAction(api.tremendous.redeemRewardAction);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button size="lg" disabled={hasRedeemed}>
+          <Text>{hasRedeemed ? "Redeemed" : "Redeem Tokens"}</Text>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-[95vw] gap-5">
+        <DialogHeader>
+          <DialogTitle>{productName}</DialogTitle>
+          {hasRedeemed ? (
+            <View className="mt-6 gap-6">
+              <Text className="text-center text-lg font-semibold">
+                Congratulations!
+              </Text>
+              <CircleCheckBig className="mx-auto text-green-500" size={55} />
+              <View className="gap-2">
+                <Text className="text-center text-lg font-semibold">
+                  Your ${unitValue} Gift Card is Ready!
+                </Text>
+                <Text className="text-center">
+                  Check your email for the redemption link and instructions.
+                </Text>
+              </View>
+            </View>
+          ) : (
+            <>
+              <Text className="mt-3">
+                Unit Price:{" "}
+                <Text className="text-primary">${unitValue} CAD</Text>
+              </Text>
+              <View className="mt-2 max-h-[300px]">
+                <FlatList
+                  data={productSkus}
+                  keyExtractor={(item) => item.toString()}
+                  ItemSeparatorComponent={() => <Separator />}
+                  renderItem={({ item }) => (
+                    <Pressable
+                      disabled={isRedeeming}
+                      onPress={() => setUnitValue(item)}
+                      className="flex flex-row items-center justify-between p-4"
+                    >
+                      <View className="flex-1 flex-row justify-between">
+                        <Text
+                          className={cn(item === unitValue && "text-primary")}
+                        >
+                          ${item} CAD
+                        </Text>
+                        {item === unitValue ? <CircleCheckBig /> : <Circle />}
+                      </View>
+                    </Pressable>
+                  )}
+                />
+              </View>
+            </>
+          )}
+        </DialogHeader>
+        {hasRedeemed ? (
+          <DialogClose>
+            <Button onPress={() => setOpen(false)}>
+              <Text>Close</Text>
+            </Button>
+          </DialogClose>
+        ) : (
+          <DialogFooter>
+            <Button
+              disabled={isRedeeming}
+              onPress={async () => {
+                setIsRedeeming(true);
+                await redeemReward({
+                  productId,
+                  name: username,
+                  email,
+                  denomination: unitValue,
+                });
+                setIsRedeeming(false);
+                setHasRedeemed(true);
+              }}
+            >
+              <Text>{isRedeeming ? "Redeeming..." : "Redeem"}</Text>
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 }
