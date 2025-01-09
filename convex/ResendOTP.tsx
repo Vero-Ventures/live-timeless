2a4e51fb39c4ba3f8220f795dd06ab9528d72314
Michael Lei <michaelleie0ca@gmail.com>
2025-01-08 17:04:35 -0800

    Revert back otp code

--- convex/ResendOTP.tsx
+++ convex/ResendOTP.tsx
@@ -1,16 +1,14 @@
 import { Email } from "@convex-dev/auth/providers/Email";
 import { Resend as ResendAPI } from "resend";
-// import { alphabet, generateRandomString } from "oslo/crypto";
+import { alphabet, generateRandomString } from "oslo/crypto";
 import LTLoginOTP from "./emails/LTLoginOTP";
 
 export const ResendOTP = Email({
   id: "resend-otp",
   apiKey: process.env.AUTH_RESEND_KEY,
-  // maxAge: 60 * 15, // 15 minutes
-  maxAge: 43200, // 1 month
+  maxAge: 60 * 15, // 15 minutes
   async generateVerificationToken() {
-    // return generateRandomString(8, alphabet("0-9"));
-    return "88888888";
+    return generateRandomString(8, alphabet("0-9"));
   },
   async sendVerificationRequest({ identifier: email, provider, token }) {
     const resend = new ResendAPI(provider.apiKey);
